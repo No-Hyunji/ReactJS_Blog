2ad7bc2ded2480a98a3715b77acd78b751b94d3c
No-Hyunji <kiekuy47@gmail.com>
2021-02-22 15:23:03 +0900

    button-modal

--- src/App.js
+++ src/App.js
@@ -14,31 +14,49 @@ function App() {
 
   let [modal, modal변경] = useState(false);
 
+  let [누른제목, 누른제목변경] = useState(0);
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
 
-      {글제목.map(function (글) {
+      {글제목.map(function (글, i) {
         return (
           <div className="list">
-            <h3>{글}</h3>
+            <h3 onClick={()=>{누른제목변경(i)}}>
+              {글}
+              <span
+                onClick={() => {
+                  따봉변경(따봉 + 1)
+                }}
+              >
+                👍
+              </span>
+              {따봉}
+            </h3>
             <p>2월 8일 발행</p>
             <hr />
           </div>
         );
       })}
 
-      {modal === true ? <Modal /> : null}
+      <button onClick={() => { 누른제목변경(0) }} > 1 </button>
+      <button onClick={() => { 누른제목변경(1) }} > 2 </button>
+      <button onClick={() => { 누른제목변경(2) }} > 3 </button>
+      <button onClick={() => { modal변경(!modal)}}>모달 </button>
+
+      {modal === true ? (
+        <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
+      ) : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{props.글제목[props.누른제목]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
